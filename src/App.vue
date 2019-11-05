457abbae822bb2fc4c81bd4dba1d0a40fa5f616b
hanhongda <hansir1993@outlook.com>
2019-11-05 21:17:08 +0800

    前三章

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <span class="text icon-A"></span>
+    <span class="text icon-bookmark"></span>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  export default {
 
-#nav {
-  padding: 30px;
-}
+  }
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > 50 ? 50 : fontSize
+    html.style.fontSize = fontSize + 'px'
+  })
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+  @import './assets/styles/global.scss';
+  @import './assets/styles/icon.css';
+  .text {
+    font-size: px2rem(20);
+    color: coral;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="read"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Epub from 'epubjs'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  global.epub = Epub
+  export default {
+    name: 'home',
+    mounted () {
+      this.book = new Epub('/完美世界.epub')
+      // console.log(this.book)
+      this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      }).display()
+    }
   }
-}
 </script>
